05bac8406f7b84b5de6d343d9223e3353aac110c
shiloalkobi <165279280+shiloalkobi@users.noreply.github.com>
2025-10-29 17:10:39 +0200

    Delete src directory

--- src/app/home/layout.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactNode } from "react";
-
-export default function HomeLayout({
-  children,
-}: {
-  children: ReactNode;
-}) {
-  return <main className="min-h-screen">{children}</main>;
-}
--- src/lib/utils.ts
+++
@@ -1,5 +0,0 @@
-export function cn(
-  ...values: Array<string | number | false | null | undefined>
-) {
-  return values.filter(Boolean).join(" ");
-}
